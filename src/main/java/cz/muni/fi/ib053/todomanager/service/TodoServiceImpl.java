cea80dea622978bb82d7aa2856ccd555613efcd3
Matus Racek <445411@mail.muni.cz>
2020-03-28 13:35:07 +0100

    logging works, some meaningful log messages needs to be added

--- src/main/java/cz/muni/fi/ib053/todomanager/service/TodoServiceImpl.java
+++ src/main/java/cz/muni/fi/ib053/todomanager/service/TodoServiceImpl.java
@@ -4,6 +4,8 @@ import cz.muni.fi.ib053.todomanager.entity.Task;
 import cz.muni.fi.ib053.todomanager.entity.User;
 import cz.muni.fi.ib053.todomanager.repository.TaskRepository;
 import cz.muni.fi.ib053.todomanager.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @Service
 public class TodoServiceImpl implements TodoService {
+        // todo log something
+        private static final Logger LOG = LoggerFactory.getLogger(TodoServiceImpl.class);
         private final UserRepository userRepository;
         private final TaskRepository taskRepository;
 
@@ -23,6 +27,7 @@ public class TodoServiceImpl implements TodoService {
         }
 
         public Boolean login(String username, String password) {
+                LOG.info("Logging works");
                 User user = userRepository.findByUsername(username);
                 if (user == null) {
                         return false;
